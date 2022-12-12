8fb961eb15928533bb4fddcf746aba81129623af
Blaz <bone.blaz@gmail.com>
2022-12-12 19:24:53 +0100

    3a done

--- assignment5/3a.py
+++ assignment5/3a.py
@@ -1,33 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def triangulate(points1, points2, cal_mtx1, cal_mtx2):
-  T = np.array([[-1, 0, 0],
-                [0, 0, 1],
-                [0, -1, 0]])
-  # convert points to homogeneous coordinates
-  points1 = np.array([points1[0], points1[1], 1])
-  points2 = np.array([points2[0], points2[1], 1])
+  ## vecotr product in matrix form
+def vec_prod(a):
+  return np.array([[0, -a[2], a[1]], [a[2], 0, -a[0]], [-a[1], a[0], 0]])
 
-  # points to matrix
-  a= points1
-  points1 = np.array([[0, -1, a[1]],
-                      [1, 0, -a[0]],
-                      [-a[1], a[0], 0]]) 
-  b = points2
-  points2 = np.array([[0, -1, b[1]],
-                      [1, 0, -b[0]],
-                      [-b[1], b[0], 0]])
-  A1 =points1 @ cal_mtx1
-  A2 = points2 @ cal_mtx2
-  A = np.vstack((A1, A2))
-  
-  # decompose A into U, D, V
-  U, D, V = np.linalg.svd(A)
-  # X is the last column of V
-  X = V[-1,:]
-  X = X / X[-1]
-  return X[:3]
+def triangulate(points_1, points_2, camera1, camera2):  
+  # homogunes points
+  points_1 = np.hstack((points_1, np.ones((points_1.shape[0], 1))))
+  points_2 = np.hstack((points_2, np.ones((points_2.shape[0], 1))))
+
+  # triangulate
+  P = []
+  for one, two in zip(points_1, points_2):
+    A1 = vec_prod(one) @ camera1
+    A2 = vec_prod(two) @ camera2
+    A = np.vstack((A1, A2))
+    U, S, V = np.linalg.svd(A)
+    X = V[-1, :]
+    X = X / X[-1]
+    P.append(X[:3])
+  return np.array(P)
+
 
 #callibration matrix
 camera1 = np.loadtxt("./data/epipolar/house1_camera.txt")
@@ -39,23 +33,35 @@ points_1 = points[:, :2]
 points_2 = points[:, 2:]
 
 
-fig = plt.figure(figsize=(4,4))
+#plot
+fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
+T = np.array([[-1, 0, 0], [0, 0, -1], [0, 1, 0]])
+
 # triangulate points
-X = []
+X = triangulate(points_1, points_2, camera1, camera2)
+ 
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+X = (T @ X.T).T
+ax.scatter(X[:, 0], X[:, 1], X[:, 2])
+
+for i in range(X.shape[0]):
+  ax.text(X[i, 0], X[i, 1], X[i, 2], str(i))
+
+
+# plot pictures
+# # plot points 
+fig, ax = plt.subplots(1, 2)
+ax[0].imshow(plt.imread("./data/epipolar/house1.jpg"))
+ax[0].scatter(points_1[:, 0], points_1[:, 1], c='r', s=1)
+ax[1].imshow(plt.imread("./data/epipolar/house2.jpg"))
+ax[1].scatter(points_2[:, 0], points_2[:, 1], c='r', s=1)
+
 for i in range(points_1.shape[0]):
-  X.append(triangulate(points_1[i], points_2[i], camera1, camera2))
-  point = X[i]
-  ax.scatter(point[0], point[1], point[2])
+  ax[0].text(points_1[i, 0], points_1[i, 1], str(i))
+  ax[1].text(points_2[i, 0], points_2[i, 1], str(i))
 
 
-
-X = np.array(X)
-print(X.shape)
-print(X)
-
-# plt.scatter(X[:,0],X[:,1],X[:,2])
-# plt.show()
-# ax.scatter(2,3,4) # plot the point (2,3,4) on the figure
 plt.show()
